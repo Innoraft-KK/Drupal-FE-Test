b12fe1c587fb8dc89e2c1b3385e9eecab556ac76
Innoraft-KK <kartik.khandelwal@innoraft.com>
2023-10-16 19:14:58 +0530

    2nd part of test day

--- web/themes/custom/custom_subtheme/js/slider.js
+++ web/themes/custom/custom_subtheme/js/slider.js
@@ -1,18 +1,18 @@
 (function ($, Drupal) {
     Drupal.behaviors.mySlickSlider = {
       attach: function (context, settings) {
-        // Initialize Slick Slider on the wrapper element.
-        /* $('.view-content').slick({ */
         $(' .view-content').slick({
           dots: true,
           infinite: true,
           speed: 300,
           slidesToShow: 1,
-         /*  autoplay: true, */
           autoplaySpeed: 2000,
           adaptiveHeight: true,
-          prevArrow: '<i class="fa-solid fa-chevron-left fa-2xl"></i>',
-          nextArrow: '<i class="fa-solid fa-chevron-right  fa-2xl"></i>',
+          prevArrow: '<i class="fa-solid fa-chevron-left fa-lg"></i>',
+          nextArrow: '<i class="fa-solid fa-chevron-right  fa-lg"></i>',
+        });
+        $('.carousel__text__title:contains("IND")').each(function() {
+          $(this).prev('.hastag').css('display', 'inline');
         });
       }
     };
